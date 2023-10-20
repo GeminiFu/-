48afb8a253062594e781c34a454e194a214d74a1
Albus_Fu <b123963@yahoo.com.tw>
2023-10-20 12:45:56 +0800

    加入專案檔案。

---
+++ 分數圓餅圖/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace 分數圓餅圖
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.num_Radius = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.num_Section = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rb_Draw_2 = new System.Windows.Forms.RadioButton();
+            this.rb_Draw_1 = new System.Windows.Forms.RadioButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Radius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Section)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Radius：";
+            // 
+            // num_Radius
+            // 
+            this.num_Radius.Location = new System.Drawing.Point(101, 58);
+            this.num_Radius.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.num_Radius.Name = "num_Radius";
+            this.num_Radius.Size = new System.Drawing.Size(52, 22);
+            this.num_Radius.TabIndex = 4;
+            this.num_Radius.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.num_Radius.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Section：";
+            // 
+            // num_Section
+            // 
+            this.num_Section.Location = new System.Drawing.Point(101, 84);
+            this.num_Section.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.num_Section.Name = "num_Section";
+            this.num_Section.Size = new System.Drawing.Size(52, 22);
+            this.num_Section.TabIndex = 4;
+            this.num_Section.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.num_Section.ValueChanged += new System.EventHandler(this.num_Section_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rb_Draw_2);
+            this.panel1.Controls.Add(this.rb_Draw_1);
+            this.panel1.Location = new System.Drawing.Point(269, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(99, 55);
+            this.panel1.TabIndex = 5;
+            // 
+            // rb_Draw_2
+            // 
+            this.rb_Draw_2.AutoSize = true;
+            this.rb_Draw_2.Location = new System.Drawing.Point(20, 31);
+            this.rb_Draw_2.Name = "rb_Draw_2";
+            this.rb_Draw_2.Size = new System.Drawing.Size(57, 16);
+            this.rb_Draw_2.TabIndex = 0;
+            this.rb_Draw_2.TabStop = true;
+            this.rb_Draw_2.Text = "Draw 2";
+            this.rb_Draw_2.UseVisualStyleBackColor = true;
+            this.rb_Draw_2.CheckedChanged += new System.EventHandler(this.rb_Draw_2_CheckedChanged);
+            // 
+            // rb_Draw_1
+            // 
+            this.rb_Draw_1.AutoSize = true;
+            this.rb_Draw_1.Checked = true;
+            this.rb_Draw_1.Location = new System.Drawing.Point(20, 6);
+            this.rb_Draw_1.Name = "rb_Draw_1";
+            this.rb_Draw_1.Size = new System.Drawing.Size(57, 16);
+            this.rb_Draw_1.TabIndex = 0;
+            this.rb_Draw_1.TabStop = true;
+            this.rb_Draw_1.Text = "Draw 1";
+            this.rb_Draw_1.UseVisualStyleBackColor = true;
+            this.rb_Draw_1.CheckedChanged += new System.EventHandler(this.rb_Draw_1_CheckedChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(38, 138);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 420);
+            this.splitContainer1.SplitterDistance = 421;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 420);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.ClientSizeChanged += new System.EventHandler(this.pictureBox1_ClientSizeChanged);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(444, 420);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 588);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.num_Section);
+            this.Controls.Add(this.num_Radius);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.num_Radius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Section)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown num_Radius;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown num_Section;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rb_Draw_1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton rb_Draw_2;
+    }
+}
+
---
+++ 分數圓餅圖/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+
+namespace 分數圓餅圖
+{
+    public partial class Form1 : Form
+    {
+        float radius = 100;
+        int section = 3;
+        Point center;
+        Graphics g;
+        Bitmap bmp;
+        List<float> angleList = new List<float>();
+        bool clickDown1 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+
+            center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            num_Radius.Value = (decimal)radius;
+            num_Section.Value = section;
+
+            DrawCircle();
+        }
+
+        private void DrawCircle()
+        {
+            Pen penBlack = new Pen(Color.Black);
+            float p0X, p0Y, p1X, p1Y;
+            double radian;
+
+            g.Clear(Color.White); // 畫布清空
+            angleList.Clear(); // 角度清空
+
+            // 畫一個圓
+            g.DrawEllipse(penBlack, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+
+            // 畫線
+            for (int i = 0; i < section; i++)
+            {
+                angleList.Add(360f * i / section);
+                radian = DegreeToRad(angleList[i]); // 角度轉弧度
+                p0X = center.X; // 中心點
+                p0Y = center.Y; // 中心點
+                p1X = center.X + ((float)Math.Cos(radian) * radius); // Cosine 算臨邊 也就是 X 座標
+                p1Y = center.Y + ((float)Math.Sin(radian) * radius); // Sine 算臨邊 也就是 Y 座標
+                g.DrawLine(penBlack, p0X, p0Y, p1X, p1Y); // 畫線
+            }
+
+            if (rb_Draw_1.Checked)
+            {
+                pictureBox1.Image = bmp;
+            }
+            else if (rb_Draw_2.Checked)
+            {
+                pictureBox2.Image = bmp;
+            }
+        }
+
+        private double DegreeToRad(float degree)
+        {
+            double rad;
+
+            rad = degree * (2f * (float)Math.PI) / 360f; // 360度 = 2PI，角度轉弧度是 x * 2PI / 360
+
+            return rad;
+        }
+
+        private float RadToDegree(double rad)
+        {
+            float degree;
+
+            degree = (float)rad * 360f / (2f * (float)Math.PI); // 360度 = 2PI，弧度轉角度是 x * 360 / 2PI
+
+            return degree;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            radius = (int)num_Radius.Value;
+
+            DrawCircle();
+        }
+
+        private void num_Section_ValueChanged(object sender, EventArgs e)
+        {
+            section = (int)num_Section.Value;
+
+            DrawCircle();
+        }
+
+        private void rb_Draw_1_CheckedChanged(object sender, EventArgs e)
+        {
+            DrawCircle();
+        }
+
+        private void rb_Draw_2_CheckedChanged(object sender, EventArgs e)
+        {
+            DrawCircle();
+        }
+
+        private void pictureBox1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+
+            center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            DrawCircle();
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.X > (center.X - radius) && e.X < (center.X + radius)) // 判斷 X 軸是否在圓裡面
+            {
+                if (e.Y > (center.Y - radius) && e.Y < (center.Y + radius)) // 判斷 Y 軸是否在圓裡面
+                {
+                    clickDown1 = true;
+
+                    float side = e.X - center.X; // 臨邊
+                    float opponent = center.Y - e.Y; // 對邊
+                    float incline = (float)Math.Sqrt(Math.Pow((double)side, 2) + Math.Pow((double)opponent, 2)); // 斜邊
+                    float angle;
+
+                    angle = RadToDegree(Math.Asin(opponent / incline)); // 用反三角函數得到弧度，再把弧度轉成角度
+
+                    // 要把反三角函數得到的角度轉成 360 度的系統
+                    if (side < 0 && opponent >= 0)
+                    {
+                        angle = 90 + (90 - angle); // 把左上四分之一個角度轉過來加上 90 度
+                    }
+                    else if (side <= 0 && opponent < 0)
+                    {
+                        angle = 180 - angle; // angle 是負的
+                    }
+                    else if (side >= 0 && opponent < 0)
+                    {
+                        angle = 270 + 90 + angle; // angle 是負的
+                    }
+
+                    Console.WriteLine($"Click angle is {angle}");
+
+                    // 前面畫圓的時候，順便紀錄每條線的角度
+                    // 在這邊找到第一個大於點擊的角度，點擊的位置就在這條線和上一條線中間
+                    // 沒找到的話，代表是在最後一塊
+                    float index = angleList.FindIndex(x => x > angle); 
+                    Brush brush = new SolidBrush(Color.FromArgb(255, 0, 0));
+                    float p0X, p0Y, p1X, p1Y;
+
+                    p0X = center.X - radius; // 圓心
+                    p0Y = center.Y - radius; // 圓心
+
+                    if (index > 0)
+                    {
+                        // 填滿圓，畫角度是順時鐘，所以加個負號變成逆時鐘
+                        g.FillPie(brush, p0X, p0Y, 2 * radius, 2 * radius, -angleList[(int)index - 1], -360 / section);
+                    }
+                    else
+                    {
+                        g.FillPie(brush, p0X, p0Y, 2 * radius, 2 * radius, -angleList[angleList.Count - 1], -360 / section);
+                    }
+
+                    if (rb_Draw_1.Checked)
+                    {
+                        pictureBox1.Image = bmp; // 畫上圖案
+                    }
+                    else if (rb_Draw_2.Checked)
+                    {
+                        pictureBox2.Image = bmp;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clickDown1)
+            {
+                if (e.X > (center.X - radius) && e.X < (center.X + radius)) // 判斷 X 軸是否在圓裡面
+                {
+                    if (e.Y > (center.Y - radius) && e.Y < (center.Y + radius)) // 判斷 Y 軸是否在圓裡面
+                    {
+                        // 在圓裡面
+                        float side = e.X - center.X;
+                        float opponent = center.Y - e.Y;
+                        float incline = (float)Math.Sqrt(Math.Pow((double)side, 2) + Math.Pow((double)opponent, 2));
+                        float angle;
+
+                        angle = RadToDegree(Math.Asin(opponent / incline));
+                        Console.WriteLine($"Cosine angle is {angle}");
+                        if (side < 0 && opponent >= 0)
+                        {
+                            angle = 90 + (90 - angle); // 把左上四分之一個角度轉過來加上 90 度
+                        }
+                        else if (side <= 0 && opponent < 0)
+                        {
+                            angle = 180 - angle; // angle 是負的
+                        }
+                        else if (side >= 0 && opponent < 0)
+                        {
+                            angle = 270 + 90 + angle; // angle 是負的
+                        }
+                        Console.WriteLine($"Click angle is {angle}");
+
+                        float index = angleList.FindIndex(x => x > angle);
+                        Brush brush = new SolidBrush(Color.FromArgb(255, 0, 0));
+
+                        if (index > 0)
+                        {
+                            g.FillPie(brush, center.X - radius, center.Y - radius, 2 * radius, 2 * radius, -angleList[(int)index - 1], -360 / section);
+                        }
+                        else
+                        {
+                            g.FillPie(brush, center.X - radius, center.Y - radius, 2 * radius, 2 * radius, -angleList[angleList.Count - 1], -360 / section);
+                        }
+
+                        if (rb_Draw_1.Checked)
+                        {
+                            pictureBox1.Image = bmp;
+                        }
+                        else if (rb_Draw_2.Checked)
+                        {
+                            pictureBox2.Image = bmp;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            clickDown1 = false;
+        }
+    }
+}

40ba7f67c0171337415720e41fc9bb22fb6790ed
Albus_Fu <b123963@yahoo.com.tw>
2023-10-22 20:21:36 +0800

    文字中文化

--- 分數圓餅圖/Form1.Designer.cs
+++ 分數圓餅圖/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbl_Section2 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.num_Radius)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Section)).BeginInit();
             this.panel1.SuspendLayout();
@@ -54,22 +55,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(46, 60);
+            this.label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(228, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 12);
+            this.label3.Size = new System.Drawing.Size(73, 21);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Radius：";
+            this.label3.Text = "半徑：";
             // 
             // num_Radius
             // 
-            this.num_Radius.Location = new System.Drawing.Point(101, 58);
+            this.num_Radius.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num_Radius.Location = new System.Drawing.Point(307, 60);
             this.num_Radius.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.num_Radius.Name = "num_Radius";
-            this.num_Radius.Size = new System.Drawing.Size(52, 22);
+            this.num_Radius.Size = new System.Drawing.Size(52, 33);
             this.num_Radius.TabIndex = 4;
             this.num_Radius.Value = new decimal(new int[] {
             100,
@@ -81,22 +84,24 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(46, 86);
+            this.label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(228, 101);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 12);
+            this.label4.Size = new System.Drawing.Size(73, 21);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Section：";
+            this.label4.Text = "分母：";
             // 
             // num_Section
             // 
-            this.num_Section.Location = new System.Drawing.Point(101, 84);
+            this.num_Section.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.num_Section.Location = new System.Drawing.Point(307, 99);
             this.num_Section.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.num_Section.Name = "num_Section";
-            this.num_Section.Size = new System.Drawing.Size(52, 22);
+            this.num_Section.Size = new System.Drawing.Size(52, 33);
             this.num_Section.TabIndex = 4;
             this.num_Section.Value = new decimal(new int[] {
             100,
@@ -109,20 +114,21 @@
             // 
             this.panel1.Controls.Add(this.rb_Draw_2);
             this.panel1.Controls.Add(this.rb_Draw_1);
-            this.panel1.Location = new System.Drawing.Point(269, 25);
+            this.panel1.Location = new System.Drawing.Point(38, 64);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(99, 55);
+            this.panel1.Size = new System.Drawing.Size(161, 56);
             this.panel1.TabIndex = 5;
             // 
             // rb_Draw_2
             // 
             this.rb_Draw_2.AutoSize = true;
-            this.rb_Draw_2.Location = new System.Drawing.Point(20, 31);
+            this.rb_Draw_2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rb_Draw_2.Location = new System.Drawing.Point(3, 28);
             this.rb_Draw_2.Name = "rb_Draw_2";
-            this.rb_Draw_2.Size = new System.Drawing.Size(57, 16);
+            this.rb_Draw_2.Size = new System.Drawing.Size(154, 25);
             this.rb_Draw_2.TabIndex = 0;
             this.rb_Draw_2.TabStop = true;
-            this.rb_Draw_2.Text = "Draw 2";
+            this.rb_Draw_2.Text = "畫第二個圓餅";
             this.rb_Draw_2.UseVisualStyleBackColor = true;
             this.rb_Draw_2.CheckedChanged += new System.EventHandler(this.rb_Draw_2_CheckedChanged);
             // 
@@ -130,12 +136,13 @@
             // 
             this.rb_Draw_1.AutoSize = true;
             this.rb_Draw_1.Checked = true;
-            this.rb_Draw_1.Location = new System.Drawing.Point(20, 6);
+            this.rb_Draw_1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rb_Draw_1.Location = new System.Drawing.Point(3, 3);
             this.rb_Draw_1.Name = "rb_Draw_1";
-            this.rb_Draw_1.Size = new System.Drawing.Size(57, 16);
+            this.rb_Draw_1.Size = new System.Drawing.Size(154, 25);
             this.rb_Draw_1.TabIndex = 0;
             this.rb_Draw_1.TabStop = true;
-            this.rb_Draw_1.Text = "Draw 1";
+            this.rb_Draw_1.Text = "畫第一個圓餅";
             this.rb_Draw_1.UseVisualStyleBackColor = true;
             this.rb_Draw_1.CheckedChanged += new System.EventHandler(this.rb_Draw_1_CheckedChanged);
             // 
@@ -207,11 +214,22 @@
             this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
             this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "分數圓餅圖產生器";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(934, 588);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.num_Section);
@@ -219,7 +237,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "分數圓餅圖產生器";
             this.Shown += new System.EventHandler(this.Form1_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.num_Radius)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.num_Section)).EndInit();
@@ -251,6 +269,7 @@
         private System.Windows.Forms.RadioButton rb_Draw_2;
         private System.Windows.Forms.Label lbl_Section1;
         private System.Windows.Forms.Label lbl_Section2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
